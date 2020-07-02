3637b24ac0d9191ff51eb99247deba7e3eecda50
Taewoong Park <taewoong_park@naver.com>
2020-07-02 23:21:54 +0900

    chartjs practice

--- covid19_visualization/src/App.vue
+++ covid19_visualization/src/App.vue
@@ -1,15 +1,72 @@
 <template>
-  <div id="app">
-  
+  <div id="app" class="container">
+    <div class="row mt-5" v-if="arrPositive.length > 0">
+      <div class="col">
+        <h2>Positive Increase per Day</h2>
+        <line-chart :chartData="arrPositiveInc" :options="chartOptions" label="Positive Increase"></line-chart>
+        
+        <h2>Positive Total</h2>
+        <line-chart :chartData="arrPositive" :options="chartOptions" label="Positive"></line-chart>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import moment from 'moment'
+import LineChart from './components/LineChart'
 
 export default {
   name: 'App',
-  components: {
+  components:{
+    LineChart
+  },
+  data() {
+    return {
+      arrPositive: [],
+      arrHospitalized: [],
+      arrInICU: [],
+      arrOnVentilator: [],
+      arrRecovered: [],
+      arrDeath: [],
 
+      arrDeathInc: [],
+      arrPositiveInc: [],
+
+      chartOptions: {
+          responsive: true,
+          maintainAspectRatio: false,
+      }
+    }
+  }, 
+  async created() {
+    const { data } = await axios.get("https://covidtracking.com/api/us/daily")
+    console.log(data)
+    data.forEach(d => {
+      const date = moment(d.date, "YYYYMMDD").format("MM/DD")
+
+      const {
+        positive,
+        hospitalizedCurrently,
+        inIcuCurrently,
+        onVentilatorCurrently,
+        recovered,
+        death,
+        positiveIncrease,
+        deathIncrease,
+      } = d
+
+      this.arrPositive.push({date, total: positive})
+      this.arrHospitalized.push({date, total: hospitalizedCurrently})
+      this.arrInICU.push({date, total: inIcuCurrently})
+      this.arrOnVentilator.push({date, total: onVentilatorCurrently})
+      this.arrRecovered.push({date, total: recovered})
+      this.arrDeath.push({date, total: death})
+      this.arrPositiveInc.push({date, total: positiveIncrease})
+      this.arrDeathInc.push({date, total: deathIncrease})
+    })
   }
 }
 </script>
@@ -22,5 +79,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.col {
+  height: 100vh;
 }
 </style>
--- covid19_visualization/src/main.js
+++ covid19_visualization/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import moment from 'moment'
+
+moment.locale('ko')
 
 Vue.config.productionTip = false
 
